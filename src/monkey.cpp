a10113d74db8e72c84655ed1020b1ec9a57271c3
tuomina9 <akseli.tuominen@aalto.fi>
2023-10-25 21:48:45 +0300

    First SFML test

--- src/monkey.cpp
+++ src/monkey.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "monkey.hpp"
 
+#include<iostream>
+
 int main() {
-  mainhpp();
-  std::cout << "Hello World!";
-  return 0;
+    if (__cplusplus == 202101L) std::cout << "C++23";
+    else if (__cplusplus == 202002L) std::cout << "C++20";
+    else if (__cplusplus == 201703L) std::cout << "C++17";
+    else if (__cplusplus == 201402L) std::cout << "C++14";
+    else if (__cplusplus == 201103L) std::cout << "C++11";
+    else if (__cplusplus == 199711L) std::cout << "C++98";
+    else std::cout << "pre-standard C++." << __cplusplus;
+    std::cout << "\n"; 
 }
