03db3c21ad497adb1b4d2fb71c8355fd28e29ef6
tuomina9 <akseli.tuominen@aalto.fi>
2023-11-02 18:01:03 +0200

    First Graphics

---
+++ src/Graphics/graphicFunctions.cpp
@@ -0,0 +1,40 @@
+#include "graphicFunctions.h"
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
+void drawTiles(sf::RenderWindow &window,
+                const int tileSize,const int windowWidth, const int windowHeight){
+                    
+    const int mapWidth = windowWidth / tileSize;
+    const int mapHeight = windowHeight / tileSize;
+    sf::RectangleShape mapTiles[mapWidth][mapHeight];
+
+    for (int x = 0; x < mapWidth; x++) {
+        for (int y = 0; y < mapHeight; y++) {
+            sf::RectangleShape& tile = mapTiles[x][y];
+            tile.setSize(sf::Vector2f(tileSize, tileSize));
+
+            // Assign colors to different tiles
+            if (x == mapWidth / 2) {
+                tile.setFillColor(sf::Color::White);
+            } else {
+                // Randomly decide whether to make it green or blue
+                if (std::rand() % 2 == 0) {
+                    tile.setFillColor(sf::Color::Green); // Green for plots of land
+                } else {
+                    tile.setFillColor(sf::Color::Blue); // Blue for lakes
+                    tile.setOutlineColor(sf::Color::Black);
+                    tile.setOutlineThickness(0.8);
+                }
+            }
+
+            tile.setPosition(x * tileSize, y * tileSize);
+        }
+    }
+            for (int x = 0; x < mapWidth; x++) {
+            for (int y = 0; y < mapHeight; y++) {
+                window.draw(mapTiles[x][y]);
+            }
+        }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include "Graphics/graphicFunctions.cpp"
+#include <ctime>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+    const int windowWidth = 800;
+    const int windowHeight = 600;
+    const int tileSize = 50; // Size of each tile in pixels
 
-    while (window.isOpen())
-    {
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Colored Tile Map");
+
+
+    drawTiles(window,tileSize,windowWidth,windowHeight);
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        
+
+        
+
         window.display();
     }
 
