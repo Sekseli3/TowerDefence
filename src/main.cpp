0f76526cc5a59f64d4f936d1182a696650eea6a1
tuomina9 <akseli.tuominen@aalto.fi>
2023-11-06 15:03:46 +0200

    addTower created

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,14 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Colored Tile Map");
 
     //draw tiles
-    //drawTiles(window,tileSize,windowWidth,windowHeight);
+    drawTiles(window,tileSize,windowWidth,windowHeight);
 
     //Create a arbitary tile to put the tower to
     sf::Vector2f tilePosition(7 * tileSize, 7 * tileSize);
     Tile tile(tilePosition, sf::Color::Black, tileSize);
     //Add tower to tile
     addTower(window,tile);
-    drawGraphics(window,tileSize,windowWidth, windowHeight);
+    //drawGraphics(window,tileSize,windowWidth, windowHeight);
 
     while (window.isOpen()) {
         sf::Event event;
